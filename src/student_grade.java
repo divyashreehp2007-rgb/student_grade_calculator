063f3509510e85fe2c696b42e9b22c1a543bcdab
divyashreehp2007-rgb <divyashreehp2007@gmail.com>
2026-07-28 17:54:36 +0530

    update student grade calculator file

--- src/student_grade.java
+++ src/student_grade.java
@@ -5,41 +5,42 @@ import static java.lang.Integer.sum;
 public class student_grade {
     static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("-----student grade calculator-----");
-        System.out.print("enter your name: ");
+        System.out.println("--------student grade calculator--------");
+        System.out.print("Name      : ");
         String name = input.nextLine();
-        System.out.print("enter your usn: ");
+        System.out.print("USN       : ");
         char USN = input.next().trim().charAt(0);
-        System.out.print("enter the first subject mark: ");
+        System.out.print("Subject 1 : ");
         int m1 = input.nextInt();
-        System.out.print("enter the second subject mark: ");
+        System.out.print("Subject 2 : ");
         int m2 = input.nextInt();
-        System.out.print("enter the third subject mark: ");
+        System.out.print("Subject 3 : ");
         int m3 = input.nextInt();
-        System.out.print("enter the fourth subject mark: ");
+        System.out.print("Subject 4 : ");
         int m4 = input.nextInt();
-        System.out.print("enter the fifth subject mark: ");
+        System.out.print("Subject 5 : ");
         int m5= input.nextInt();
         int total = m1 + m2 + m3 + m4 + m5;
+        System.out.print("Total     : " + total);
         float percentage = (total / 5.0f);
-        System.out.print("grade: " + percentage);
+        System.out.print("\nPercentage: " + percentage + "%");
         if (m1 > 35 && m2 > 35 &&  m3 > 35 &&  m4 > 35 &&  m5 > 35) {
-            System.out.println("\nResult: PASS");
+            System.out.println("\nResult    : PASS");
         } else {
-            System.out.println("\nresult: FAIL");
+            System.out.println("\nresult    : FAIL");
         }
         if (percentage >= 90 ) {
-            System.out.println("Grade: A+");
+            System.out.println("Grade     : A+");
         } else if (percentage >= 80) {
-            System.out.println("Grade: A");
+            System.out.println("Grade     : A");
         } else if (percentage >= 70 ) {
-            System.out.println("Grade: B");
+            System.out.println("Grade     : B");
         } else if (percentage >= 60 ) {
-            System.out.println("Grade: C");
+            System.out.println("Grade     : C");
         } else if (percentage >= 50 ) {
-            System.out.println("Grade: D");
+            System.out.println("Grade     : D");
         } else if (percentage >= 35 ) {
-            System.out.println("Grade: F");
+            System.out.println("Grade     : F");
         }
 
     }
